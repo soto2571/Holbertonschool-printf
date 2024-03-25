3ff3c0220e2061980d29550c61c567aaac524db5
soto2571 <sebastian.soto1649@gmail.com>
2024-03-25 15:37:04 -0400

    Testing this technique

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_putchar - write the c character
+ *@c: The character to print
+ *
+ *Return: 1 or -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
