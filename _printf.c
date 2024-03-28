96ff2b7aed685d3934f78ab67dcb4f2badaa6a44
soto2571 <sebastian.soto1649@gmail.com>
2024-03-28 13:40:10 -0400

    Commiting changes to _printf.c before pulling

--- _printf.c
+++ _printf.c
@@ -22,7 +22,6 @@ int _printf(const char * const format, ...)
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-Here:
 while (format[i] != '\0')
 {
 	j = 13;
@@ -32,7 +31,6 @@ while (format[i] != '\0')
 		{
 			len += m[j].f(args);
 			i = i + 2;
-			goto Here;
 
 		}
 		j--;
